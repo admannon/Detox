183cf18d379bd187b2a80f68bc7d595180d7f95f
peerakarn.j <peerakarn.j@lmwn.com>
2022-09-13 09:58:31 +0700

    Update Android Espresso

--- generation/utils/downloadEspresso.js
+++ generation/utils/downloadEspresso.js
@@ -3,5 +3,5 @@ const downloadFile = require('./downloadFile');
 
 module.exports = function downloadEspresso(fullyQualifiedClass) {
   const path = fullyQualifiedClass.replace(/\./g, '/');
-  return downloadFile(`https://raw.githubusercontent.com/android/android-test/androidx-test-1.1.0/espresso/core/java/${path}.java`);
+  return downloadFile(`https://raw.githubusercontent.com/android/android-test/androidx-test-1.4.0/espresso/core/java/${path}.java`);
 };
